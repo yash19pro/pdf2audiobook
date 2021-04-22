1a3f46a130f42c81aba1da20341f9c7c20c347b4
PatilOjas <ojaspatil1901@gmail.com>
2021-04-22 11:02:32 +0530

    Threaded editorial processor

--- editorial2audio.py
+++ editorial2audio.py
@@ -1,15 +1,18 @@
 from pdf2image import convert_from_path
 import cv2
 import pytesseract
-import pyttsx3              # Default rate of reading is 200 word per minute
+import pyttsx3  # Default rate of reading is 200 word per minute
 import os
 import shutil
 import re
-
+from google_drive_downloader import GoogleDriveDownloader as Gdd
+import threading
+import time
 
 # Access tesseract OCR by providing its location
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 # above line should be commented/uncommented depending on OS
+
 
 # To create a proper path for creation of temporary directory named 'imgs' which gets deleted later
 path = "imgs"
@@ -18,30 +21,45 @@ path = "imgs"
 engine = pyttsx3.init()
 engine.setProperty('rate', 185)
 
-
-# Fetch PDF
-pdfname = str(input("Enter name of PDF: "))
-padfpath = "{}.pdf".format(pdfname)
-
+# PDF input mode
+# Make necessary changes "HERE!!!" while integrating
+pdfpath = str()
+pdfname = str()
+mode = int(input("#####\tMENU\t#####\n1: Input using Google Drive link\n2: Upload PDF\nYour choice: "))
+if mode == 1:
+    drive_link = input("Enter drive link for The Hindu Newspaper pdf: ").strip()
+    file_name = input("Save as (*.pdf): ").strip()
+    file_id = re.split(r'/', drive_link)
+    Gdd.download_file_from_google_drive(file_id=file_id[5], dest_path='./data/{}.pdf'.format(file_name), showsize=True)
+    pdfname = file_name
+    pdfpath = './data/{}.pdf'.format(file_name)
+elif mode == 2:
+    pdfname = str(input("Enter name of PDF: "))
+    pdfpath = "{}.pdf".format(pdfname)
+else:
+    print("Invalid input!!!")
+    exit(0)
 
 # Convert PDF pages to images
 # next line should be commented on MacOS and uncommented on Windows
-# pages = convert_from_path(poppler_path="C:\\poppler-21.02.0\\Library\\bin", pdf_path=padfpath, dpi=300, fmt="jpeg", grayscale=True, size=(2921, 3449))
+pages = convert_from_path(poppler_path="C:/poppler-21.02.0/Library/bin", pdf_path=pdfpath, dpi=300, fmt="jpeg", grayscale=True, size=(2921, 3449))
 # next line should be commented on Windows and uncommented on MacOS
-pages = convert_from_path('IAG.pdf')
+# pages = convert_from_path(pdfpath)
+
 
 # Create imgs folder at specified path if it doesn't exist. If it exists, then delete it and create once again
 try:
     os.mkdir(path)
+    os.mkdir(pdfname)
 except FileExistsError:
     shutil.rmtree(path, ignore_errors=True)
+    shutil.rmtree(pdfname, ignore_errors=True)
     os.mkdir(path)
-
+    os.mkdir(pdfpath)
 
 # Text is used to find whether the page has editorials or not
 text = str()
 print("Number of pages: ", len(pages))
-
 
 # Saves the images in jpeg format
 for i in range(len(pages)):
@@ -49,10 +67,93 @@ for i in range(len(pages)):
     pages[i].save(imgname, 'JPEG')
 
 
+def left_column_editorial(img):
+    # Processes left column editorial
+    print("Column editorial processing started")
+    img_edi1 = img[361: 3393, 81: 777]
+    img_edi1 = cv2.resize(src=img_edi1, dsize=(400, 2000), fx=10, fy=0.01)
+    ret, img_threshold = cv2.threshold(img_edi1, 170, 255, cv2.THRESH_BINARY)
+    edi1 = pytesseract.image_to_string(img_threshold)
+
+    # Removes unwanted newline characters so that speech quality can be improved
+    edi1 = re.sub(r"\b\n", " ", edi1)
+    edi1 = re.sub(r"\n\b", " ", edi1)
+    edi1 = re.sub(r"-\s", "", edi1)
+
+    engine.save_to_file(edi1, "{}/Edi1.mp3".format(pdfname))
+    engine.runAndWait()
+    print("Column editorial processing done!!!")
+
+
+def upper_right_editorial(img_edi2):
+    # Finds the end of picture in upper right editorial and covers it with black colour so that if any text is there, it will not interfere with actual editorial.
+    print("Upper right editorial processing started")
+    img_temp = img_edi2[300:, 623:900]
+    edi_data = pytesseract.image_to_data(img_temp)
+    imgHeight, imgWidth, _ = img_temp.shape
+    tops = []
+    for num, specs in enumerate(edi_data.splitlines()):
+        specs_list = specs.split()
+        if num != 0:
+            if len(specs_list) == 12:
+                x, y, w, h = int(specs_list[6]), int(specs_list[7]), int(specs_list[8]), int(specs_list[9])
+                if h <= 20:
+                    tops.append(y)
+                cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255))
+    edi2_cut_at = min(tops) + 300
+    img_edi2[93:edi2_cut_at, 610:900] = [0, 0, 0]
+
+    edi2 = pytesseract.image_to_string(img_edi2)
+
+    # Removes unwanted newline characters so that speech quality can be improved
+    edi2 = re.sub(r"\b\n", " ", edi2)
+    edi2 = re.sub(r"\n\b", " ", edi2)
+    edi2 = re.sub(r"-\s", "", edi2)
+
+    cv2.imwrite('{}/edi2.jpeg'.format(pdfname), img_edi2)
+    cv2.waitKey(0)
+    engine.save_to_file(edi2, "{}/Edi2.mp3".format(pdfname))
+    engine.runAndWait()
+    print("Upper right editorial processing done!!!")
+
+
+def lower_right_editorial(img_edi3):
+    # Finds the end of picture in lower right editorial and covers it with black colour
+    print("Lower right editorial processing started")
+    img_temp = img_edi3[300:, 623:900]
+    edi_data = pytesseract.image_to_data(img_temp)
+    imgHeight, imgWidth, _ = img_temp.shape
+    tops = []
+    for num, specs in enumerate(edi_data.splitlines()):
+        specs_list = specs.split()
+        if num != 0:
+            if len(specs_list) == 12:
+                x, y, w, h = int(specs_list[6]), int(specs_list[7]), int(specs_list[8]), int(specs_list[9])
+                if h <= 20:
+                    tops.append(y)
+                cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255))
+    edi3_cut_at = min(tops) + 300
+    img_edi3[113:edi3_cut_at, 610:900] = [0, 0, 0]
+
+    edi3 = pytesseract.image_to_string(img_edi3)
+
+    # Removes unwanted newline characters so that speech quality can be improved
+    edi3 = re.sub(r"\b\n", " ", edi3)
+    edi3 = re.sub(r"\n\b", " ", edi3)
+    edi3 = re.sub(r"-\s", "", edi3)
+
+    cv2.imwrite('{}/edi3.jpeg'.format(pdfname), img_edi3)
+    cv2.waitKey(0)
+    # os.system('start edi3.jpeg')
+    engine.save_to_file(edi3, "{}/Edi3.mp3".format(pdfname))
+    engine.runAndWait()
+    print("Lower right editorial processing done!!!")
+
+
 # Reads upper left corner of each page.
 title = str()
 for i in range(len(pages)):
-    img = cv2.imread("imgs\\{}{}.jpeg".format(pdfname, i))
+    img = cv2.imread("imgs/{}{}.jpeg".format(pdfname, i))
     img_title = img[80:140, 180:650]
 
     # Converts image to grey scale and then thresholds it.
@@ -69,20 +170,9 @@ for i in range(len(pages)):
 
     if title == "EDITORIAL":
 
-        # Processes left column editorial
-        img_edi1 = img[361: 3393, 81: 777]
-        img_edi1 = cv2.resize(src=img_edi1, dsize=(400, 2000), fx=10, fy=0.01)
-        ret, img_threshold = cv2.threshold(img_edi1, 170, 255, cv2.THRESH_BINARY)
-        edi1 = pytesseract.image_to_string(img_threshold)
-
-        # Removes unwanted newline characters so that speech quality can be improved
-        edi1 = re.sub(r"\b\n", " ", edi1)
-        edi1 = re.sub(r"\n\b", " ", edi1)
-        edi1 = re.sub(r"-\s", "", edi1)
-
-        print(edi1)
-        engine.save_to_file("Edi1.mp3", edi1)
-        engine.runAndWait()
+        # Processing left column editorial using thread to achieve speed
+        t1 = threading.Thread(target=left_column_editorial, args=(img,))
+        t1.start()
 
         # Finds the point from where the lower right editorial starts
         img_temp = img[800:, 800:2850]
@@ -99,77 +189,37 @@ for i in range(len(pages)):
                     if h >= 40:
                         tops.append(y)
         cut_at = min(tops) + 800
-        print("\nThis is cut point: ", cut_at)
         img_edi2 = cv2.resize(src=img[200:cut_at - 2, 800:2850], dsize=(1500, 1000), fx=0.4, fy=0.1)
-        cv2.imshow('Edi_2', img_edi2)
-        cv2.waitKey(0)
-        img_edi3 = cv2.resize(src=img[cut_at:2825, 800:2850], dsize=(1500, 1000), fx=0.4, fy=0.1)
-        cv2.imshow("Edi_3", img_edi3)
-        cv2.waitKey(0)
 
-        # Finds the end of picture in upper right editorial and covers it with black colour so that if any text is there, it will not interfere with actual editorial.
-        img_temp = img_edi2[300:, 623:900]
-        edi_data = pytesseract.image_to_data(img_temp)
+        # Finds the point where the lower right editorial finishes
+        img_temp = img[cut_at + 500:, 800:2850]
+        data = pytesseract.image_to_data(img_temp)
         imgHeight, imgWidth, _ = img_temp.shape
-        edi2_cut_at = 0
-        tops = []
-        for num, specs in enumerate(edi_data.splitlines()):
+        top = a = b = x = y = w = h = 0
+        for num, specs in enumerate(data.splitlines()):
             specs_list = specs.split()
             if num != 0:
+                b = int(specs_list[6])
                 if len(specs_list) == 12:
                     word = specs_list[11]
                     x, y, w, h = int(specs_list[6]), int(specs_list[7]), int(specs_list[8]), int(specs_list[9])
-                    if h <= 20:
-                        tops.append(y)
-                    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255))
-        edi2_cut_at = min(tops) + 300
-        print("\nThis is cut point for Editorial 2: ", edi2_cut_at)
-        img_edi2[93:edi2_cut_at, 610:900] = [0, 0, 0]
+                    if b - a >= 1000:
+                        top = y
+                        break
+            a = b
+        cut_at_lower = y + cut_at - 40
+        img_edi3 = cv2.resize(src=img[cut_at:cut_at_lower, 800:2850], dsize=(1500, 1000), fx=0.4, fy=0.1)
 
-        edi2 = pytesseract.image_to_string(img_edi2)
+        t2 = threading.Thread(target=upper_right_editorial, args=(img_edi2,))
+        t2.start()
 
-        # Removes unwanted newline characters so that speech quality can be improved
-        edi2 = re.sub(r"\b\n", " ", edi2)
-        edi2 = re.sub(r"\n\b", " ", edi2)
-        edi2 = re.sub(r"-\s", "", edi2)
+        time.sleep(5)
 
-        cv2.imwrite('edi2.jpeg', img_edi2)
-        cv2.waitKey(0)
-        os.system('start edi2.jpeg')
-        engine.save_to_file("Edi2.mp3", edi2)
-        engine.runAndWait()
+        t3 = threading.Thread(target=lower_right_editorial, args=(img_edi3,))
+        t3.start()
 
-        # Finds the end of picture in lower right editorial and covers it with black colour
-        img_temp = img_edi3[300:, 623:900]
-        edi_data = pytesseract.image_to_data(img_temp)
-        imgHeight, imgWidth, _ = img_temp.shape
-        edi3_cut_at = 0
-        tops = []
-        for num, specs in enumerate(edi_data.splitlines()):
-            specs_list = specs.split()
-            if num != 0:
-                if len(specs_list) == 12:
-                    word = specs_list[11]
-                    x, y, w, h = int(specs_list[6]), int(specs_list[7]), int(specs_list[8]), int(specs_list[9])
-                    if h <= 20:
-                        tops.append(y)
-                    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255))
-        edi3_cut_at = min(tops) + 300
-        print("\nThis is cut point for Editorial 3: ", edi3_cut_at)
-        img_edi3[113:edi3_cut_at, 610:900] = [0, 0, 0]
-
-        edi3 = str(pytesseract.image_to_string(img_edi3))
-
-        # Removes unwanted newline characters so that speech quality can be improved
-        edi3 = re.sub(r"\b\n", " ", edi3)
-        edi3 = re.sub(r"\n\b", " ", edi3)
-        edi3 = re.sub(r"-\s", "", edi3)
-
-        cv2.imwrite('edi3.jpeg', img_edi3)
-        cv2.waitKey(0)
-        os.system('start edi3.jpeg')
-        engine.save_to_file("Edi3.mp3", edi3)
-        engine.runAndWait()
-
+        # t1.join()
+        # t2.join()
+        # t3.join()
 
 shutil.rmtree(path, ignore_errors=True)
