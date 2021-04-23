6e58d1862fc2a09db9cd4f8718a3904c74a7ab5f
PatilOjas <ojaspatil1901@gmail.com>
2021-04-23 23:04:38 +0530

    Chapter based PDF to image Chapter based image to audio

--- images2audio.py
+++ images2audio.py
@@ -6,6 +6,7 @@ import pyttsx3
 import os
 import re
 import time
+import ast
 
 text = str()
 
@@ -26,13 +27,14 @@ def next_img(name):
 
 
 class Image2audio:
-    def __init__(self, name, speechrate, start, end):
+    def __init__(self, name, chapter_name, speechrate, start, end):
         self.speechrate = speechrate
         self.text = str()
         self.start = start
         self.end = end
         self.pdfpath = os.path.dirname(__file__)
         self.pdfname = name
+        self.chapter_name = chapter_name
 
         # Initializing TTS engine
         self.engine = pyttsx3.init()
@@ -40,7 +42,7 @@ class Image2audio:
 
     def converter(self):
         global text
-        img = cv2.imread("{}/pdf2audiobook/media/{}/page{}.jpg".format(self.pdfpath, self.pdfname, 0))
+        img = cv2.imread("{}/pdf2audiobook/media/{}/{}/page{}.jpg".format(self.pdfpath, self.pdfname, self.chapter_name, 0))
         # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         # ret, img_threshold = cv2.threshold(gray, 170, 255, cv2.THRESH_BINARY)
         text = pytesseract.image_to_string(img)
@@ -52,18 +54,27 @@ class Image2audio:
         # scanning the images and extracting the text from the image
         for i in range(self.start, self.end+1):
             if i < self.end:
-                b = "{}/pdf2audiobook/media/{}/page{}.jpg".format(self.pdfpath, self.pdfname, str(i + 1 - self.start))
+                b = "{}/pdf2audiobook/media/{}/{}/page{}.jpg".format(self.pdfpath, self.pdfname, self.chapter_name, str(i + 1 - self.start))
                 t = threading.Thread(target=next_img, args=(b,))
                 print(b)
                 t.start()
             self.engine.say("page " + str(i - self.start) + ' started!')
             self.engine.runAndWait()
             # self.engine.say(text)
-            self.engine.save_to_file(text, "{}/pdf2audiobook/media/{}/page{}.mp3".format(self.pdfpath, self.pdfname, i - self.start))
+            self.engine.save_to_file(text, "{}/pdf2audiobook/media/{}/{}/page{}.mp3".format(self.pdfpath, self.pdfname,
+                                                                                            self.chapter_name, i - self.start))
             self.engine.runAndWait()
             print('page ' + str(i - self.start) + ' done')
             if i < self.end:
                 t.join()
 
-a = Image2audio("IAG", 500, 15, 17)
-a.converter()
+
+f = open('{}/pdf2audiobook/media/{}/Index.txt'.format(os.path.dirname(__file__), "Eat_That_Frog"))
+makemyindex = f.read()
+makemyindex = ast.literal_eval(makemyindex)
+
+index_keys = list(makemyindex.keys())
+index_values = list(makemyindex.values())
+for x in range(len(index_values)):
+    a = Image2audio("Eat_That_Frog", index_keys[x], 500, index_values[x][0], index_values[x][1])
+    a.converter()
