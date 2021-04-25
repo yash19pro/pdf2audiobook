c87588113f52d8a22fe96a1d119c0736af6b7d04
PatilOjas <ojaspatil1901@gmail.com>
2021-04-25 15:09:27 +0530

    Editorial module

--- training.py
+++ training.py
@@ -1,5 +1,5 @@
 print("Hi Ojas")
 x = input("Press Enter:")
 from editorial2audio import Editorial2audiobook
-x = Editorial2audiobook(mode=1, drive_link="https://drive.google.com/file/d/1_5CeqKW6xFZniJuV2lBM0bOVF2ue9fMp/view?usp=drivesdk",
-        file_name="TH23April")
+x = Editorial2audiobook(mode=1, drive_link="https://drive.google.com/file/d/1a4U0GsOiAtmJDFM3dbQUechOqVSRM7Fy/view?usp=drivesdk",
+        file_name="TH25April")
