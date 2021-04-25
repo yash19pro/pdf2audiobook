057d31674222d4f3ca4417598855ab61809ad67a
yash19_pro <65077864+yash19pro@users.noreply.github.com>
2021-04-25 11:28:44 +0530

    respective folders created

--- pdf2audiobook/convert/models.py
+++ pdf2audiobook/convert/models.py
@@ -11,3 +11,28 @@ class Books(models.Model):
 	def delete(self, *args, **kwargs):
 		self.file.delete()
 		super().delete(*args, **kwargs)
+
+class Editorial(models.Model):
+	title = models.CharField(max_length=100)
+	file = models.FileField(upload_to='editorials/')
+
+	def __str__(self):
+		return self.title
+
+	def delete(self, *args, **kwargs):
+		self.file.delete()
+		super().delete(*args, **kwargs)
+
+class Audiobooks(models.Model):
+	title = models.ForeignKey(Books, on_delete=models.CASCADE)
+	audiobook = models.FileField(upload_to="audiobook_books/")
+
+	def __str__(self):
+		return self.title
+
+class Editorial_Audiobooks(models.Model):
+	title = models.ForeignKey(Editorial, on_delete=models.CASCADE)
+	audiobook = models.FileField(upload_to="audiobook_editorials/")
+
+	def __str__(self):
+		return self.title
