569b73c17bf11c3b4aa8126e6544e207f2e9585d
yash19_pro <65077864+yash19pro@users.noreply.github.com>
2021-04-25 11:11:46 +0530

    Changes saved us

--- pdf2audiobook/pdf2audiobook/urls.py
+++ pdf2audiobook/pdf2audiobook/urls.py
@@ -30,5 +30,5 @@ urlpatterns = [
     path('about_us/', about_us, name = 'about-us-page'),
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
